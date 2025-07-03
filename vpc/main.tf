d0e95015778d1a474875aa94255df8066378dbe3
Tanseer <khantanseer43@gmail.com>
2025-07-03 17:02:32 +0530

    added vpc config

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,6 +1,7 @@
 # AWS VPC
 resource "aws_vpc" "prod-vpc" {
     cidr_block = var.cidr_block
+    instance_tenancy = "default"
     tags = {
       Name = "prod-vpc"
       Environment = "Production"
