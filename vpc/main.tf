18364194969b0bcce5546841d79d44d70cde5444
Tanseer <khantanseer43@gmail.com>
2025-07-04 16:50:56 +0530

    code for creating subnets

--- vpc/main.tf
+++ vpc/main.tf
@@ -9,3 +9,26 @@ resource "aws_vpc" "prod-vpc" {
 }
 
 # AWS Subnet
+resource "aws_subnet" "public_subnet" {
+    count = length(var.public_subnet_cidrs)
+    vpc_id = aws_vpc.prod-vpc.id
+    cidr_block = var.public_subnet_cidrs[count.index]
+    availability_zone = element(var.availability_zones, count.index)
+    tags = {
+      Name = "public-subnet-${count.index + 1}"
+      Environment = "Production"
+    }
+  
+}
+
+# AWS Subnet
+resource "aws_subnet" "private_subnet" {
+    count = length(var.private_subnet_cidrs)
+    vpc_id = aws_vpc.prod-vpc.id
+    cidr_block = var.private_subnet_cidrs[count.index]
+    availability_zone = element(var.availability_zones, count.index)
+    tags = {
+      Name = "private-subnet-${count.index + 1}"
+      Environment = "Production"
+    }
+}
